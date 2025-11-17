0f2b22910d6966d49659f04edf17d9a18e6fd2be
DavidScranton <98499686+DavidScranton@users.noreply.github.com>
2025-11-17 11:02:55 -0600

    Added very basic widget tests for screens

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -5,11 +5,37 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cadence/screens/activity_detail_screen.dart';
+import 'package:cadence/screens/finish_activity_screen.dart';
+import 'package:cadence/screens/home_screen.dart';
+import 'package:cadence/screens/recorder_screen.dart';
+import 'package:cadence/screens/settings_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Basic test', (WidgetTester tester) async {
+  testWidgets('HomeScreen test', (WidgetTester tester) async {
     // TODO: Add proper tests with mocked providers
+    await tester.pumpWidget(const HomeScreen());
+    expect(1, equals(1));
+  });
+  testWidgets('SettingsScreen test', (WidgetTester tester) async {
+    // TODO: Add proper tests with mocked providers
+    await tester.pumpWidget(const SettingsScreen());
+    expect(1, equals(1));
+  });
+  testWidgets('RecorderScreen test', (WidgetTester tester) async {
+    // TODO: Add proper tests with mocked providers
+    await tester.pumpWidget(const RecorderScreen());
+    expect(1, equals(1));
+  });
+  testWidgets('ActivityDetailScreen test', (WidgetTester tester) async {
+    // TODO: Add proper tests with mocked providers
+    //await tester.pumpWidget(const ActivityDetailScreen(activity: activity));
+    expect(1, equals(1));
+  });
+  testWidgets('FinishActivityScreen test', (WidgetTester tester) async {
+    // TODO: Add proper tests with mocked providers
+    await tester.pumpWidget(const FinishActivityScreen(formattedTime: '1:24:49', formattedDistance: '5 mi', activityName: "testActivity"));
     expect(1, equals(1));
   });
 }
