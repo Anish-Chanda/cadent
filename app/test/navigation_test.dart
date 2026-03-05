02f108416cdbe517e1395b20a25f98423a198833
Blake Clabaugh <clab22@iastate.edu>
2026-03-05 00:39:04 -0600

    had to fix navigation tests

--- app/test/navigation_test.dart
+++ app/test/navigation_test.dart
@@ -5,37 +5,50 @@ import 'package:provider/provider.dart';
 import 'package:mocktail/mocktail.dart';
 
 import 'Mocks/mock_auth_provider.dart';
+
 import 'package:cadence/providers/auth_provider.dart';
 import 'package:cadence/providers/activities_provider.dart';
 import 'package:cadence/providers/app_settings_provider.dart';
 import 'package:cadence/providers/theme_provider.dart';
+
 import 'package:cadence/utils/app_theme.dart';
 import 'package:cadence/screens/recorder_screen.dart';
 import 'package:cadence/screens/settings_screen.dart';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+
+  setUp(() {
+    SharedPreferences.setMockInitialValues({});
+  });
+
+  Widget buildTestApp(AuthProvider authProvider) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
+        ChangeNotifierProvider(create: (_) => ActivitiesProvider()),
+        ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider(AppTheme.lightTheme),
+        ),
+      ],
+      child: const MaterialApp(
+        home: MainLayout(),
+      ),
+    );
+  }
+
   testWidgets('Initial Page is Home Screen', (WidgetTester tester) async {
     final authProvider = MockAuthProvider();
+
     when(() => authProvider.status).thenReturn(AuthStatus.authenticated);
     when(() => authProvider.isLoading).thenReturn(false);
     when(() => authProvider.serverUrl).thenReturn('http://test.com');
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
-          ChangeNotifierProvider(create: (_) => ActivitiesProvider()),
-          ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
-          ChangeNotifierProvider(
-            create: (_) => ThemeProvider(AppTheme.lightTheme),
-          ),
-        ],
-        child: const MaterialApp(
-          home: MainLayout(),
-        ),
-      ),
-    );
-    await tester.pump();
+
+    await tester.pumpWidget(buildTestApp(authProvider));
+    await tester.pumpAndSettle();
 
     expect(find.byType(HomeScreen), findsOneWidget);
     expect(find.byType(SettingsScreen), findsNothing);
@@ -43,88 +56,60 @@ void main() {
 
   testWidgets('Change Screen to Settings', (WidgetTester tester) async {
     final authProvider = MockAuthProvider();
+
     when(() => authProvider.status).thenReturn(AuthStatus.authenticated);
     when(() => authProvider.isLoading).thenReturn(false);
     when(() => authProvider.serverUrl).thenReturn('http://test.com');
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
-          ChangeNotifierProvider(create: (_) => ActivitiesProvider()),
-          ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
-          ChangeNotifierProvider(
-            create: (_) => ThemeProvider(AppTheme.lightTheme),
-          ),
-        ],
-        child: const MaterialApp(
-          home: MainLayout(),
-        ),
-      ),
-    );
-    await tester.pump();
 
-    await tester.tap(find.byIcon(Icons.settings));
+    await tester.pumpWidget(buildTestApp(authProvider));
     await tester.pumpAndSettle();
+
+    final settingsIcon = find.byIcon(Icons.settings);
+
+    expect(settingsIcon, findsOneWidget);
+
+    await tester.tap(settingsIcon);
+    await tester.pumpAndSettle();
+
     expect(find.byType(HomeScreen), findsNothing);
     expect(find.byType(SettingsScreen), findsOneWidget);
   });
 
   testWidgets('Change Screen to Home', (WidgetTester tester) async {
     final authProvider = MockAuthProvider();
+
     when(() => authProvider.status).thenReturn(AuthStatus.authenticated);
     when(() => authProvider.isLoading).thenReturn(false);
     when(() => authProvider.serverUrl).thenReturn('http://test.com');
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
-          ChangeNotifierProvider(create: (_) => ActivitiesProvider()),
-          ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
-          ChangeNotifierProvider(
-            create: (_) => ThemeProvider(AppTheme.lightTheme),
-          ),
-        ],
-        child: const MaterialApp(
-          home: MainLayout(),
-        ),
-      ),
-    );
-    await tester.pump();
+
+    await tester.pumpWidget(buildTestApp(authProvider));
+    await tester.pumpAndSettle();
 
     await tester.tap(find.byIcon(Icons.settings));
     await tester.pumpAndSettle();
-    expect(find.byType(HomeScreen), findsNothing);
+
     expect(find.byType(SettingsScreen), findsOneWidget);
 
     await tester.tap(find.byIcon(Icons.home));
     await tester.pumpAndSettle();
-    expect(find.byType(SettingsScreen), findsNothing);
+
     expect(find.byType(HomeScreen), findsOneWidget);
+    expect(find.byType(SettingsScreen), findsNothing);
   });
 
   testWidgets('Change Screen to Recording', (WidgetTester tester) async {
     final authProvider = MockAuthProvider();
+
     when(() => authProvider.status).thenReturn(AuthStatus.authenticated);
     when(() => authProvider.isLoading).thenReturn(false);
     when(() => authProvider.serverUrl).thenReturn('http://test.com');
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
-          ChangeNotifierProvider(create: (_) => ActivitiesProvider()),
-          ChangeNotifierProvider(
-            create: (_) => ThemeProvider(AppTheme.lightTheme),
-          ),
-        ],
-        child: const MaterialApp(
-          home: MainLayout(),
-        ),
-      ),
-    );
-    await tester.pump();
+
+    await tester.pumpWidget(buildTestApp(authProvider));
+    await tester.pumpAndSettle();
 
     await tester.tap(find.byType(FloatingActionButton));
     await tester.pumpAndSettle();
+
     expect(find.byType(HomeScreen), findsNothing);
     expect(find.byType(SettingsScreen), findsNothing);
     expect(find.byType(RecorderScreen), findsOneWidget);
